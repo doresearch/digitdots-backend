2e85b474d5988e4515b7d6ead4cff66b21f52051
wangbo02 <wangbo02@shizhuang-inc.com>
2023-12-17 23:52:41 +0800

    feat: push 页面

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,13 +6,23 @@ const routes = [
   {
     path: '/',
     name: 'home',
+    // Wix HomePage
+    // The UK 、 USA
     component: Home,
   },
+  // 注册页面
   {
     path: '/register',
     name: 'register',
     component: Register,
-  }
+  },
+  // 老师登陆完成后选择时间
+  // 老师介绍页面（后置）
+  // 商品列表页面（UK、USA）
+  // 下单页面
+  // 订单详情
+  // 支付页面
+
 ]
 
 const router = createRouter({
