7ae241a6d59adb8e29f5fb8dea004739d91f17fa
fan <vincefanz@gmail.com>
2023-12-18 23:54:25 +0800

    feat: 🎸 home

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -2,13 +2,33 @@
   <div class="home">
     <div class="banner-content p-relative">
       <img class="banner-bg" src="@/assets/bg1.jpg" alt="" />
-      <div class="content p-absolute">
+      <div class="content p-absolute text-white">
         <div class="text-4xl font-bold">Link up & Grow up</div>
-        <div>
+        <div class="p-2 text-lg">
           where you can link up with awesome educators who <br />
           are ready to help you learn and grow!
         </div>
-        <router-link to="/">Link Now</router-link>
+        <router-link class="mt-8 px-8 py-3 rounded bg-black text-white hover:bg-black/[0.8]" to="/"
+          >Link Now</router-link
+        >
+      </div>
+    </div>
+    <div class="w-full h-16 pt-2 bg-black text-center text-white text-lg">First order get £30 off</div>
+    <div class="flex">
+      <div class="flex-1 pl-26">
+        <p class="mt-40 text-2xl">What you met</p>
+        <p class="text-6xl font-bold mt-12">
+          “Will I be able to apply to <br />
+          my dream school?”
+        </p>
+      </div>
+      <div class="flex-1 pl-18">
+        <p class="mt-120 text-2xl">What we offer</p>
+        <p class="text-6xl font-bold mt-12">
+          “Gives you a chance to talk <br />
+          directly with educators at <br />
+          your dream school.”
+        </p>
       </div>
     </div>
   </div>
@@ -31,6 +51,10 @@
     left: 0;
     width: 100%;
     height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
   }
 }
 
