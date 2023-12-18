2de827e30389b1c4839cd8da1bd8aa2471eec62b
wangbo02 <wangbo02@shizhuang-inc.com>
2023-12-18 21:28:14 +0800

    feat: 支持step 0.5 到 500

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
+const repeatMap = (times, step) => {
+  const result = {}
+  for (let i = 0; i < times; i += step) {
+    result[i] = i / 4
+  }
+  return result
+}
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.{ts,vue,js}'],
   theme: {
-    extend: {},
+    extend: {
+      width: repeatMap(500, 1),
+      margin: repeatMap(500, 1),
+      padding: repeatMap(500, 1),
+    },
   },
   plugins: [],
 }
