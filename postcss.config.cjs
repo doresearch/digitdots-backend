cc25ee20c4ce057bb9f9c7aa0052ece4aaa71e3b
fan <vincefanz@gmail.com>
2023-12-18 18:44:33 +0800

    feat: 🎸 rm postcss-pxtorem

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -3,15 +3,15 @@ module.exports = ctx => ({
   map: ctx.env === 'development' ? ctx.map : false,
   plugins: {
     autoprefixer: {},
-    'postcss-pxtorem': {
-      rootValue: 192, // 设计稿宽度的1/10
-      unitPrecision: 5,
-      propList: ['*'], // 需要做转化处理的属性，如`hight`、`width`、`margin`等，`*`表示全部
-      selectorBlackList: [],
-      replace: true,
-      mediaQuery: false,
-      minPixelValue: 0,
-      exclude: /node_modules/i,
-    },
+    // 'postcss-pxtorem': {
+    //   rootValue: 192, // 设计稿宽度的1/10
+    //   unitPrecision: 5,
+    //   propList: ['*'], // 需要做转化处理的属性，如`hight`、`width`、`margin`等，`*`表示全部
+    //   selectorBlackList: [],
+    //   replace: true,
+    //   mediaQuery: false,
+    //   minPixelValue: 0,
+    //   exclude: /node_modules/i,
+    // },
   },
 })
